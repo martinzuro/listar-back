3b9ee35fb150adc7eaed70a17bc9446115db82b6
Martin zuro <martinzuro@gmail.com>
2023-03-10 10:21:14 -0300

    add ssl:true to database config

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const config = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         port: process.env.DB_PORT,
+        ssl: true
     },
     listPerPage: 10,
   };
